11311a4df3d812c654aa0d7b1530bb6d81893d63
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-10-27 13:15:08 -0400

    Add PasswordSalt to Auth table

--- backend/Api/Models/Auth.cs
+++ backend/Api/Models/Auth.cs
@@ -4,6 +4,7 @@ namespace Api.Models
     {
         public int UserId { get; set; }
         public required string PasswordHash { get; set; }
+        public required string PasswordSalt { get; set; }
         public DateTime LastLogin { get; set; }
         public int FailedAttempts { get; set; }
         public DateTime LockUntil { get; set; }
