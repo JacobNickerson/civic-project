a1e96959cb62206448bcbbc2b6a6ff61fdba84af
Andrew Sklyarov <andrew.v.sklyarov@gmail.com>
2025-10-29 17:29:12 -0400

    Add explicit CSS logic for each router element to keep CSS settings in them from overwriting each other (each page's stylesheet is loaded simultaneously)

--- frontend/src/HomeScreen.jsx
+++ frontend/src/HomeScreen.jsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomeScreen.css';
 
 function HomeScreen() {
   return (
-    <div>
+    <div className='home-screen'>
         <div className='welcome-container'>
             <h1 className='welcome-msg'>Welcome to TownVoice</h1>
             <h2 className='welcome-msg-subtitle'>Connecting communities with the governments that represent them.</h2>
--- frontend/src/SignInScreen.jsx
+++ frontend/src/SignInScreen.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import './SignInScreen.css';
 
 function SignInScreen() {
     return(
-        <div>
+        <div className='sign-in-screen'>
             <h1>Sign in screen</h1>
         </div>
     );
