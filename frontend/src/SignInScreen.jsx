d5f20b89bf74978f46051e1527859ed9b1818ed9
Andrew Sklyarov <andrew.v.sklyarov@gmail.com>
2025-10-28 20:37:55 -0400

    Added routing logic to sign in screen (sign-in screen incomplete)

---
+++ frontend/src/SignInScreen.jsx
@@ -0,0 +1,11 @@
+import './SignInScreen.css';
+
+function SignInScreen() {
+    return(
+        <div>
+            <h1>Sign in screen</h1>
+        </div>
+    );
+}
+
+export default SignInScreen;
